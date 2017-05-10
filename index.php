4857c5662a9f020f92f1dea54b0f331ce4709475
Weixinghua <wxh52794@gmail.com>
2017-05-10 12:08:07 -0700

    third change

--- index.php
+++ index.php
@@ -6,16 +6,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/index.css">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap.wei.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
-  <style>
-    /* Remove the navbar's default margin-bottom and rounded borders */ 
-    
-    
-    /* Add a gray background color and some padding to the footer */
- 
-  </style>
 </head>
 <body>
 
@@ -41,35 +34,46 @@
   <h4 class="text-center">Or search by category:</h4>  
   <form action="index.php" method="post">
   <div class="row"> 
-    <div class="col-xs-6 col-md-4">
+    <div class="col-xs-6 col-md-4 container_overlay">
                
         
 		 <input type="image" src="images/meat.jpg" alt="SubmitImg" style="width:100%" id = "cMeat" class="img-responsive" name = "category" value = "meat">
-		<!--<img src="images/meat.jpg" class="img-responsive" style="width:100%" alt="Image" id = "cMeat">-->
+		<a href="index.html"><div class="overlay">
+		<div class="text_overlay">Meat</div>
+		</div></a>
       </div>
    
-    <div class="col-xs-6 col-md-4"> 
-
-        
+    <div class="col-xs-6 col-md-4 container_overlay"> 
+  
  <input type="image" src="images/vege.jpg" alt="SubmitImg" style="width:100%" id = "cVege" class="img-responsive" name = "category" value = "vege">
-     
+     <div class="overlay_1">
+		<div class="text_overlay">Vege</div>
+		</div>
     </div>
-    <div class="col-xs-6 col-md-4"> 
+    <div class="col-xs-6 col-md-4 container_overlay"> 
 
        
  <input type="image" src="images/dairy.jpg" alt="SubmitImg" style="width:100%" id = "cDairy" class="img-responsive" name = "category" value = "dairy">
-      
+      <div class="overlay_2">
+		<div class="text_overlay">Dairy</div>
+		</div>
     </div>
   </div>
   </form>
 </div><br>
 
 
-<footer class="container-fluid text-center">
+<footer>
 <div id="conFooter">
             <a href="#" id="conAboutus">About Us</a>
-			
+			<a href="https://www.facebook.com">
+         <img src="images/fb.png" alt="fb">
+         </a><a href="https://www.instagram.com/?hl=en">
+         <img src="images/ig.png" alt="ig">
+         </a>
             <p>Copyright &copy; 2017 Test company</p>
+			
+			
 </div>
 </footer>
 

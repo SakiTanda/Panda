8315e516da555aa558c4e0dae274429afaceb07c
Xin-Heng <skartfreedom@msn.com>
2017-05-10 15:15:03 -0700

    fix problem

--- js/xin.js
+++ js/xin.js
@@ -1,19 +1,12 @@
-function $(id){
-	var element = document.getElementById(id);
-	if(element== null)
-		alert('program error: '+ id +'does not exist.');
-	return element;
-}
-
 function arrowchange(id){
 	
-	if($(id).getAttribute('class')=="down") {
+	if(document.getElementById(id).getAttribute('class')=="down") {
 		//alert($(id).src.substr($(id).src.length-13, 4));
-		$(id).src="images/arrow-up-3-24.png";
-		$(id).setAttribute('class', 'up');
+		document.getElementById(id).src="images/arrow-up-3-24.png";
+		document.getElementById(id).setAttribute('class', 'up');
 	} else {
 		//alert($(id).src.substr($(id).src.length-13, 4));
-		$(id).setAttribute('class', 'down');
-		$(id).src="images/arrow-down-3-24.png";
+		document.getElementById(id).setAttribute('class', 'down');
+		document.getElementById(id).src="images/arrow-down-3-24.png";
 	}
 }

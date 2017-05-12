c872830676290cb7c2e50e3ad6d6bf8bc9ddb716
Xin-Heng <skartfreedom@msn.com>
2017-05-11 23:32:20 -0700

    change js

--- js/xin.js
+++ js/xin.js
@@ -1,10 +1,10 @@
-/**
+
 function toggle(id){
 	$('#collapse'+'-'+id).slideToggle();
 		}
-**/
+
 function arrowchange(id){
-	
+	console.log(id);
 	if(document.getElementById(id).getAttribute('class')=="down") {
 		document.getElementById(id).src="images/arrow-up-3-24.png";
 		document.getElementById(id).setAttribute('class', 'up');
@@ -12,18 +12,7 @@ function arrowchange(id){
 		document.getElementById(id).setAttribute('class', 'down');
 		document.getElementById(id).src="images/arrow-down-3-24.png";
 	}
-	//toggle(id);
+	toggle(id);
 }
 
 
-$('#egg').on('click',function(){
-	$('#collapse-egg').slideToggle();
-		});
-		
-$('#beef').on('click',function(){
-	$('#collapse-beef').slideToggle();
-});
-
-$('#tomato').on('click',function(){
-	$('#collapse-tomato').slideToggle();
-});

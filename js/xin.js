43292f5a4fc3ed3e016d9d99e5e9ca4a6fb0dce1
Xin-Heng <skartfreedom@msn.com>
2017-05-11 23:07:22 -0700

    fix toggle problem

--- js/xin.js
+++ js/xin.js
@@ -1,12 +1,29 @@
+/**
+function toggle(id){
+	$('#collapse'+'-'+id).slideToggle();
+		}
+**/
 function arrowchange(id){
 	
 	if(document.getElementById(id).getAttribute('class')=="down") {
-		//alert($(id).src.substr($(id).src.length-13, 4));
 		document.getElementById(id).src="images/arrow-up-3-24.png";
 		document.getElementById(id).setAttribute('class', 'up');
 	} else {
-		//alert($(id).src.substr($(id).src.length-13, 4));
 		document.getElementById(id).setAttribute('class', 'down');
 		document.getElementById(id).src="images/arrow-down-3-24.png";
 	}
+	//toggle(id);
 }
+
+
+$('#egg').on('click',function(){
+	$('#collapse-egg').slideToggle();
+		});
+		
+$('#beef').on('click',function(){
+	$('#collapse-beef').slideToggle();
+});
+
+$('#tomato').on('click',function(){
+	$('#collapse-tomato').slideToggle();
+});
